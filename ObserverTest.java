1a7ea4b67d1d0375b9ac37275df30b8fc3210628
speram1 <90123557+speram1@users.noreply.github.com>
2021-09-05 12:00:22 -0500

    Add files via upload

---
+++ ObserverTest.java
@@ -0,0 +1,21 @@
+
+public class ObserverTest {
+
+	public static void main(String[] args) {
+		User john = new User("John");
+		User jack = new User("Jack");
+		User david = new User("David");
+
+		Netflix netflix = new Netflix();
+		netflix.subscribe(john);
+		netflix.subscribe(jack);
+		netflix.subscribe(david);
+
+		System.out.println("After subscribing 3 users ----------");
+		netflix.addMovie();
+
+		netflix.unsubscribe(jack);
+		System.out.println("\nAfter unsubscribing Jack ----------");
+		netflix.addMovie();
+	}
+}
